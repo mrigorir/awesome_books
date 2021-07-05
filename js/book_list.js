5ef5640eb720cb84c2e7ba2ecd8e44697abe4c04
Marco <marcoparra311@gmail.com>
2021-07-05 17:42:41 -0400

    fix add button

--- js/book_list.js
+++ js/book_list.js
@@ -1,6 +1,6 @@
-let addButton  = document.getElementById('add');
-let bookTitle  = document.getElementById('title').value;
-let bookAuthor = document.getElementById('author').value;
+let addButton    = document.getElementById('add');
+let removeButton = document.querySelector('.remove');
+let message      = document.getElementById('message');
 
 const books = [
   {
@@ -9,15 +9,28 @@ const books = [
   },
   {
     title: 'Book 2',
-    author: 'Author 1'
+    author: 'Author 2'
   },
   {
     title: 'Book 3',
-    author: 'Author 1'
+    author: 'Author 3'
   }
 ];
 
-addButton.on('click', function() {
-  const add_collection = Object.create(books, {title: bookTitle, author: bookAuthor });
-  books.push(add_collection);
+addButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  let bookTitle  = document.getElementById('title').value;
+  let bookAuthor = document.getElementById('author').value;
+  const add_collection = Object.assign({}, {title: bookTitle, author: bookAuthor});
+
+  if (add_collection.title == ''|| add_collection.author == '') {
+    message.innerHTML = "<p> Don't let the fields empty, please. </p>";
+    return false;
+  }else {
+    books.push(add_collection);
+    message.innerHTML = 'New book added to the collection';
+  }
+
+  console.log(books);
 });
+
