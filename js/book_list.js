30937e2942a60d955b635f21e8617c407ef0bc90
Marco <marcoparra311@gmail.com>
2021-07-06 20:23:16 -0400

    Refactor to use classes and methods

--- js/book_list.js
+++ js/book_list.js
@@ -1,77 +1,75 @@
+const message = document.getElementById('message');
+const bookList = document.getElementById('list');
+const form = document.getElementById('form');
+let books = [];
+
 class Book {
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
-  
-  addBook(obj) {
-    if (this.title === '' || this.author === '') {
+}
+
+class BookList {
+  static addBook(obj) {
+    if (obj.title === '' || obj.author === '') {
       message.innerHTML = "<p> Don't let the fields empty, please. </p>";
     } else {
       books.push(obj);
-      displayBook(this.title, this.author);
+      BookList.displayBook(obj.title, obj.author);
       message.innerHTML = 'New book added to the collection';
-      counter += 1;
       localStorage.setItem('books', JSON.stringify(books));
     }
   }
-  
-  remove(btn, position) {
+
+  static remove(btn, position) {
     books.splice(position, 1);
     btn.parentNode.remove();
     message.innerHTML = 'A book has been removed';
     localStorage.setItem('books', JSON.stringify(books));
   }
-}
 
-const newBook = new Book(this.title, this.author);
-const addButton = document.getElementById('add');
-const message = document.getElementById('message');
-const bookList = document.getElementById('list');
-const booksOnStorage = JSON.parse(localStorage.getItem('books'));
-let counter = 0;
-let books = [];
-
-function displayBook(title, author) {
-  const div = document.createElement('div');
-  div.innerHTML = `<div id="${counter}">`;
-  div.innerHTML += `<p> Book Title: ${title}</p>`;
-  div.innerHTML += `<p> Book Author: ${author}</p>`;
-  div.innerHTML += '<button class=\'remove\'>Remove</button>';
-  div.innerHTML += '<hr>';
-  div.innerHTML += '</div>';
-  bookList.appendChild(div);
-}
-
-function load() {
-  if (booksOnStorage == null) {
-    localStorage.setItem('books', JSON.stringify([]));
-  } else {
-    books = booksOnStorage;
+  static displayBook(title, author) {
+    const div = document.createElement('div');
+    div.innerHTML = `<p> Book Title: ${title}</p>
+                     <p> Book Author: ${author}</p>
+                     <button class='remove'>Remove</button>
+                     <hr>`;
+    bookList.appendChild(div);
   }
-  books.forEach((book) => {
-    displayBook(book.title, book.author);
-  });
 }
 
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.reset();
-});
+class Storage {
+  static load() {
+    const booksOnStorage = JSON.parse(localStorage.getItem('books'));
+    if (booksOnStorage == null) {
+      localStorage.setItem('books', JSON.stringify([]));
+    } else {
+      books = booksOnStorage;
+    }
+    books.forEach((book) => {
+      BookList.displayBook(book.title, book.author);
+    });
+  }
+}
 
-addButton.onclick = () => {
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
-  const newBook = new Book (bookTitle, bookAuthor);
-  newBook.addBook(newBook);
-};
+  const newBook = new Book(bookTitle, bookAuthor);
+  BookList.addBook(newBook);
+  e.target.reset();
+});
 
 bookList.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove')) {
     const btn = e.target;
     const position = Array.prototype.indexOf.call(bookList.childNodes, btn.parentNode) - 1;
-    newBook.remove(btn, position);
+    BookList.remove(btn, position);
   }
 });
 
-load();
+document.addEventListener('DOMContentLoaded', () => {
+  Storage.load();
+});
